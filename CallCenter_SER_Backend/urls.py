f9ef0c8671120abc1c51de7c09aa810c18c185db
KunCheng-He <byack-hkc@foxmail.com>
2023-11-02 18:50:30 +0800

    添加API文档

--- CallCenter_SER_Backend/urls.py
+++ CallCenter_SER_Backend/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework.documentation import include_docs_urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('docs/', include_docs_urls(title="API文档", description="API文档"))  # API文档
 ]
